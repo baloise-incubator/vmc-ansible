c9f400b37da50a9f7ef18b24edf3fa573b514532
Nikolas Philips <nikolas.philips@baloise.com>
2021-12-29 16:36:40 +0000

    Add default ansible automation for VMC

---
+++ jobs/default.groovy
@@ -0,0 +1,26 @@
+podTemplate(yaml: """
+kind: Pod
+spec:
+  containers:
+  - name: ansible
+    image: docker.io/niiku/ansible-runner-openshift:v2.11
+    command:
+    - /bin/sh 
+    - -c
+    args:
+    - cat
+    tty: true
+"""
+) {
+
+    node(POD_LABEL) {
+        stage('Run ansible') {
+            container(name: 'ansible') {
+                withVault([path: '/secret/automation/ssh-key', secretValues: [[envVar: 'ANSIBLE_SSH_KEY', vaultKey: 'ssh-key']]]) {
+                    sh 'ssh-add - <<< "$ANSIBLE_SSH_KEY"'
+                }
+                sh 'ansible-playbook -i kubevirt.yaml playbooks/default.yaml'
+            }
+        }
+    }
+}
