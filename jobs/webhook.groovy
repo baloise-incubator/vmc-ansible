78313d5c74a8145aa037d880c7bfb188ff6cb8c1
Nikolas Philips <nikolas.philips@baloise.com>
2022-01-03 22:44:03 +0000

    Add webhook pipeline

---
+++ jobs/webhook.groovy
@@ -0,0 +1,34 @@
+/* groovylint-disable CompileStatic, NoDef, UnusedVariable, VariableName, VariableTypeRequired */
+@Library('jenkins-library@master') _
+
+parsedWebhookPayload = ''
+
+pipeline {
+    agent {
+        any
+    }
+
+    triggers {
+        GenericTrigger(
+                causeString: 'Generic Cause',
+                genericVariables: [[defaultValue: '', key: 'webhookPayload', regexpFilter: '', value: '$']],
+                genericHeaderVariables: [[key: 'X-KubeVirt-Event', regexpFilter: '']],
+                regexpFilterExpression: '',
+                regexpFilterText: '',
+                token: 'kubevirt-update'
+        )
+    }
+
+    stages {
+        stage('Perform update on node') {
+            steps {
+                script {
+                    parsedWebhookPayload = readJSON text: "${webhookPayload}"
+                    def eventType = "$X-KubeVirt-Event"
+                    echo eventType
+                    echo parsedWebhookPayload
+                }
+            }
+        }
+    }
+}
