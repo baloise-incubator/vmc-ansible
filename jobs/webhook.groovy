cc9d45f025b0b5c073dc05bd908a9077a4b689af
Nikolas Philips <nikolas.philips@baloise.com>
2022-01-03 23:28:10 +0000

    Add ansible-playbook execution

--- jobs/webhook.groovy
+++ jobs/webhook.groovy
@@ -4,7 +4,26 @@
 parsedWebhookPayload = ''
 
 pipeline {
-    agent any
+    agent {
+        kubernetes {
+            idleMinutes: 10 ,
+            yaml: """
+kind: Pod
+spec:
+  containers:
+  - name: ansible
+    image: docker.io/niiku/ansible-runner-openshift:v2.9-inc-4
+    command:
+    - /entrypoint.sh
+    - /bin/sh
+    - -c
+    args:
+    - cat
+    tty: true
+"""
+            )
+        }
+    }
 
     triggers {
         GenericTrigger(
@@ -28,6 +47,16 @@ pipeline {
                     def namespace = parsedWebhookPayload['namespace'].replaceAll(hostGroupCreateRegex, "_")
                     def cluster = parsedWebhookPayload['cluster'].replaceAll(hostGroupCreateRegex, "_")
                     echo "Namespace: ${namespace}; Cluster: ${cluster}"
+                    git branch: 'main', url: 'https://github.com/baloise-incubator/vmc-ansible.git'
+                    container(name: 'ansible') {
+                        withVault([vaultSecrets: [[path: '/secret/automation/ssh-key', secretValues: [[envVar: 'ANSIBLE_SSH_KEY', vaultKey: 'ssh-key']]]]]) {
+                            sh (script: '#!/bin/sh -e\necho "$ANSIBLE_SSH_KEY" > id_rsa && chmod 600 id_rsa',returnStdout: true)
+                            sh 'pwd'
+                            sh 'ls -lah'
+                            sh "ansible-playbook -i kubevirt.yaml playbooks/default.yaml --extra-vars \"ansible_ssh_private_key_file=id_rsa\" -l namespace_${namespace} -l label_cluster_${cluster}"
+                            sh 'rm id_rsa'
+                        }
+                    }
                 }
             }
         }
